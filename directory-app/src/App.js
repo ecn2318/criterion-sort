705fcd08d15cef9d4fc6948329ee8a1b785752dc
Esther N <estheroids@gmail.com>
2020-07-06 21:37:11 -0400

    testing react-bootstrap-table-next for filterable tables

--- directory-app/src/App.js
+++ directory-app/src/App.js
@@ -1,26 +1,108 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Table from "./components/Table";
+import Wrapper from "./components/Wrapper";
+import criterion from "./criterion.json";
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import filterFactory, { selectFilter, textFilter } from 'react-bootstrap-table2-filter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const selectOptions = [
+  { value: 'feature', label: 'feature' },
+  { value: 'supplement', label: 'supplement' },
+  { value: 'short', label: 'short' },
+  { value: 'trailer', label: 'trailer' }
+];
+
+const columns = [{
+  dataField: 'ID',
+  text: 'ID',
+  sort: true,
+  filter: textFilter()
+}, {
+  dataField: 'Type',
+  text: 'Type',
+  sort: true,
+  filter: selectFilter({
+    options: selectOptions
+  })
+}, {
+  dataField: 'Title',
+  text: 'Film Title',
+  sort: true,
+  filter: textFilter()
+}, {
+  dataField: 'Director',
+  text: 'Director',
+  sort: true,
+  filter: textFilter()
+}, {
+  dataField: 'Country',
+  text: 'Country',
+  sort: true,
+  filter: textFilter()
+}, {
+  dataField: 'Year',
+  text: 'Year',
+  sort: true,
+  filter: textFilter()
+}, {
+  dataField: 'Duration',
+  text: 'Duration',
+  sort: true,
+  filter: textFilter()
+}];
+
+const defaultSorted = [{
+  dataField: 'Title',
+  order: 'asc'
+}];
+
+
+class App extends React.Component {
+  state = { criterion }
+
+  render() {
+    return (<Wrapper>
+
+      <BootstrapTable
+        bootstrap4
+        keyField="id"
+        data={criterion}
+        columns={columns}
+        defaultSorted={defaultSorted}
+        pagination={paginationFactory()}
+        filter={filterFactory()}
+      />
+
+    </Wrapper>
+
+    );
+  }
+
 }
 
+
 export default App;
+
+/*
+<Wrapper>
+{this.state.criterion.map(film => (
+  <Table
+    id={film.ID}
+    key={film.ID}
+    title={film.Title}
+    director={film.Director}
+    country={film.Country}
+    year={film.Year}
+    duration={film.Duration}
+    type={film.Type}
+    spine={film.Spine}
+  />
+   ))}
+
+    </Wrapper>
+
+  */
---
+++ directory-app/src/components/Wrapper.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "../App.css";
+
+
+function Wrapper(props) {
+    return <div className="wrapper">
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Criterion Channel</h1>
+                <p className="lead">List of videos on Criterion Channel (as of 06.21.2019)</p>
+                <hr></hr>
+                <p>data source: @morbusiff</p>
+                <p className="lead">
+                    <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                </p>
+            </div>
+        </div>
+        <br></br>
+
+        {props.children}
+    </div>
+}
+
+
+export default Wrapper;
+
+/*
+ <table className="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Spine</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Director</th>
+                    <th scope="col">Country</th>
+                    <th scope="col">Year</th>
+                    <th scope="col">Duration</th>
+                </tr>
+            </thead>
+
+            <tbody>
+
+                {props.children}
+
+            </tbody>
+        </table>
+        */
